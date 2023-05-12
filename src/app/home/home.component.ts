505a0764713668925c255ee6997b792e0fa5a17a
hoangm.ndm <hoangm.ndm@gmail.com>
2023-05-12 09:56:28 +0700

    Buổi 3: ngFor

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from '../common/product';
 import { User } from '../common/user';
 
 @Component({
@@ -12,4 +13,30 @@ export class HomeComponent {
     age: 33,
     address: 'Vietnam',
   };
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Ao khoac',
+      price: 200,
+      description: 'Mo ta cho san pham',
+      imgUrl:
+        'https://routine.vn/media/catalog/product/cache/5de180fdba0e830d350bd2803a0413e8/e/c/ecom.21.10_232__1.jpg',
+    },
+    {
+      id: '2',
+      name: 'Quan',
+      price: 300,
+      description: 'Mo ta cho san pham',
+      imgUrl:
+        'https://routine.vn/media/catalog/product/cache/5de180fdba0e830d350bd2803a0413e8/e/c/ecom.21.10_232__1.jpg',
+    },
+    {
+      id: '3',
+      name: 'Vest',
+      price: 400,
+      description: 'Mo ta cho san pham',
+      imgUrl:
+        'https://routine.vn/media/catalog/product/cache/5de180fdba0e830d350bd2803a0413e8/e/c/ecom.21.10_232__1.jpg',
+    },
+  ];
 }
