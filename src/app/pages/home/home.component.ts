6c631a2669c621df99125485b2cdab0303704678
hoangm.ndm <hoangm.ndm@gmail.com>
2023-05-12 16:43:52 +0700

    Buổi 3: ngFor

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,10 +7,30 @@ import { Product } from 'src/app/common/product';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-  product: Product = {
-    id: '1',
-    name: 'Product 1',
-    price: 100,
-    description: 'Product 1 description',
-  };
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'Product 1',
+      price: 100,
+      description: 'Product 1 description',
+      imgUrl:
+        'https://file.hstatic.net/200000053174/file/cac-loai-ao-vest-nam_07a5c0b0bfee420b8edc3225a9529187.jpg',
+    },
+    {
+      id: '2',
+      name: 'Ao khoac',
+      price: 200,
+      description: 'Product 1 description',
+      imgUrl:
+        'https://file.hstatic.net/200000053174/file/cac-loai-ao-vest-nam_07a5c0b0bfee420b8edc3225a9529187.jpg',
+    },
+    {
+      id: '3',
+      name: 'Quan bo',
+      price: 300,
+      description: 'Product 1 description',
+      imgUrl:
+        'https://file.hstatic.net/200000053174/file/cac-loai-ao-vest-nam_07a5c0b0bfee420b8edc3225a9529187.jpg',
+    },
+  ];
 }
