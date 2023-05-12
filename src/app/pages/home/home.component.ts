5df71cd6a5514e59688b64644a4aacba07465102
hoangm.ndm <hoangm.ndm@gmail.com>
2023-05-12 17:13:03 +0700

    BT handle click show()

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,12 +7,14 @@ import { Product } from 'src/app/common/product';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  show = true;
+  show(id) {}
   products: Product[] = [
     {
       id: '1',
       name: 'Product 1',
       price: 100,
-      description: 'Product 1 description',
+      description: 'lorem ipsum dolor sit amet, consectetur adip',
       imgUrl:
         'https://file.hstatic.net/200000053174/file/cac-loai-ao-vest-nam_07a5c0b0bfee420b8edc3225a9529187.jpg',
     },
@@ -20,7 +22,7 @@ export class HomeComponent {
       id: '2',
       name: 'Ao khoac',
       price: 200,
-      description: 'Product 1 description',
+      description: 'lorem ipsum',
       imgUrl:
         'https://file.hstatic.net/200000053174/file/cac-loai-ao-vest-nam_07a5c0b0bfee420b8edc3225a9529187.jpg',
     },
@@ -28,7 +30,8 @@ export class HomeComponent {
       id: '3',
       name: 'Quan bo',
       price: 300,
-      description: 'Product 1 description',
+      description:
+        'Product 1 description Product 1 descriptionProduct 1 descriptionProduct 1 description',
       imgUrl:
         'https://file.hstatic.net/200000053174/file/cac-loai-ao-vest-nam_07a5c0b0bfee420b8edc3225a9529187.jpg',
     },
