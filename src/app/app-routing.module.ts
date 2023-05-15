0fae6bfdee06c015abf517850f1f5c65efcb98d4
hoangm.ndm <hoangm.ndm@gmail.com>
2023-05-15 10:10:43 +0700

    add new pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { SaleComponent } from './pages/sale/sale.component';
+import { ServicesComponent } from './pages/services/services.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'category', component: CategoryComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'sale', component: SaleComponent },
   {
     path: '**',
     component: NotFoundComponent,
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,11 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
 import { CategoryComponent } from './pages/category/category.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { AboutComponent } from './pages/about/about.component';
+import { SaleComponent } from './pages/sale/sale.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +21,12 @@ import { NotFoundComponent } from './pages/not-found/not-found.component';
     FooterComponent,
     HomeComponent,
     CategoryComponent,
-    NotFoundComponent
+    NotFoundComponent,
+    LoginComponent,
+    RegisterComponent,
+    ServicesComponent,
+    AboutComponent,
+    SaleComponent
   ],
   imports: [
     BrowserModule,
