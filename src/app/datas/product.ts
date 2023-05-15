e2a25c9e4b1e6501671f412924875a0088b03c53
hoangm.ndm <hoangm.ndm@gmail.com>
2023-05-15 10:30:45 +0700

    Buổi 4: ngIf with data products

---
+++ src/app/datas/product.ts
@@ -0,0 +1,30 @@
+import { Product } from '../common/product';
+
+const products: Product[] = [
+  {
+    id: '1',
+    name: 'Ao khoac',
+    price: 200,
+    description: 'Mo ta cho san pham',
+    imgUrl:
+      'https://routine.vn/media/catalog/product/cache/5de180fdba0e830d350bd2803a0413e8/e/c/ecom.21.10_232__1.jpg',
+  },
+  {
+    id: '2',
+    name: 'Quan',
+    price: 300,
+    description: 'Mo ta cho san pham',
+    imgUrl:
+      'https://pos.nvncdn.net/be3159-662/ps/20220610_LL9DsoP9QvQuxTNmB5ane7UR.jpg',
+  },
+  {
+    id: '3',
+    name: 'Vest',
+    price: 400,
+    description: 'Mo ta cho san pham',
+    imgUrl:
+      'https://product.hstatic.net/1000344772/product/kx-41_1600__5__7122ccabec164ff4a14093e5b13fc350_master.jpg',
+  },
+];
+
+export default products;
