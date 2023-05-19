d75d9c9c4ecc4b0d9ad63919be8a7090359efb3e
hoangm.ndm <hoangm.ndm@gmail.com>
2023-05-19 16:25:37 +0700

    fix primeng

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ButtonModule } from 'primeng/button';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -50,7 +49,6 @@ import { FooterAdminComponent } from './admin/components/footer-admin/footer-adm
     AppRoutingModule,
     FormsModule,
     BrowserAnimationsModule,
-    ButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
