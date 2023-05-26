ccbb53b31798972a397e88b3e616a0629280d89e
hoangm.ndm <hoangm.ndm@gmail.com>
2023-05-26 18:31:16 +0700

    u

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-
+  authors = [
+    { id: '1', name: 'John' },
+    { id: '2', name: 'Hoang' },
+  ];
 }
